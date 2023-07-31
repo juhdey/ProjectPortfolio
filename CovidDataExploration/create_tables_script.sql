dba33cfc4f5b9831aff68cd9c23f21bdee024cbc
Judy He <86313579+juhdey@users.noreply.github.com>
2023-07-31 10:20:21 -0700

    Update create_tables_script.sql

--- CovidDataExploration/create_tables_script.sql
+++ CovidDataExploration/create_tables_script.sql
@@ -1,1 +1,27 @@
+USE covid_data;
 
+CREATE TABLE IF NOT EXISTS covid_death_data
+(iso_code VARCHAR(50), 
+continent VARCHAR(50), 
+location VARCHAR(50), 
+date DATE, 
+population DOUBLE, 
+new_cases DOUBLE,
+total_deaths DOUBLE, 
+new_deaths DOUBLE,
+total_cases_per_million DOUBLE,
+new_cases_per_million DOUBLE, 
+total_deaths_per_million DOUBLE, 
+new_deaths_per_million DOUBLE,
+reproduction_rate DOUBLE, 
+icu_patients DOUBLE,
+icu_patients_per_million DOUBLE, 
+hosp_patients DOUBLE,
+hosp_patients_per_million DOUBLE, 
+weekly_icu_admissions DOUBLE,
+weekly_icu_admissions_per_million DOUBLE, 
+weekly_hosp_admissions DOUBLE,
+weekly_hosp_admissions_per_million DOUBLE, 
+total_tests DOUBLE, 
+total_cases DOUBLE
+)
